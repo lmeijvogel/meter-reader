e668a0c5cecb22c7a47a98fa12c60c773c502174
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2014-03-03 07:27:56 +0100

    Add forgotten init_db.sql script

---
+++ init_db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE measurements (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  time_stamp DATETIME,
+  stroom_dal FLOAT,
+  stroom_piek FLOAT,
+  stroom_current FLOAT,
+  gas FLOAT,
+
+  diff_stroom_dal FLOAT,
+  diff_stroom_piek FLOAT,
+  diff_gas FLOAT
+);
