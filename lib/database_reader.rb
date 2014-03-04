a4085d9e857518af50ba5dc85e30453d40f7c01b
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2014-03-04 22:41:47 +0100

    Add selections and granularities for measurement selects.

--- lib/database_reader.rb
+++ lib/database_reader.rb
@@ -27,11 +27,36 @@ class DatabaseReader
     end
   end
 
-  def where
+  def day=(date)
+    date = date.to_datetime
+    self.where = "WHERE time_stamp > '#{date}' AND time_stamp < '#{date.next_day}'"
+    self.granularity = :hour
+  end
 
+  def week=(date)
+    date = date.to_datetime
+    self.where = "WHERE time_stamp > '#{date}' AND time_stamp < '#{date + 7}'"
+    self.granularity = :hour
+  end
+
+  def month=(date)
+    date = date.to_datetime
+    self.where = "WHERE time_stamp > '#{date}' AND time_stamp < '#{date.next_month}'"
+    self.granularity = :day
   end
 
   def granularity
-    "DAYOFYEAR(time_stamp), HOUR(time_stamp)"
+    case @granularity
+    when :hour
+      "DAYOFYEAR(time_stamp), HOUR(time_stamp)"
+    when :day
+      "DAYOFYEAR(time_stamp)"
+    else
+      raise "Unknown granularity for data selection: #{@granularity}"
+    end
   end
+
+  protected
+  attr_accessor :where
+  attr_writer :granularity
 end
