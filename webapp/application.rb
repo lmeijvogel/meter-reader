e0bbfec3d5ff100fbea4da37499edb298a68bb49
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2014-03-28 08:14:48 +0100

    Cache previous day results on disk.

--- webapp/application.rb
+++ webapp/application.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'mysql2'
 require 'json'
 require 'pathname'
+require 'fileutils'
 
 require_relative '../lib/database_config.rb'
 require_relative '../lib/database_reader.rb'
@@ -9,6 +10,8 @@ require_relative '../lib/database_reader.rb'
 ROOT_PATH = Pathname.new(File.join(File.dirname(__FILE__), ".."))
 
 set :bind, '0.0.0.0'
+
+FileUtils.mkdir_p(ROOT_PATH.join("tmp/cache"))
 
 database_connection = Mysql2::Client.new(DatabaseConfig.for(settings.environment))
 
@@ -21,11 +24,15 @@ get "/day/today" do
 end
 
 get "/day/:year/:month/:day" do
-  database_reader = DatabaseReader.new(database_connection)
+  day = DateTime.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
 
-  database_reader.day = DateTime.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+  cached(:day, day) do
+    database_reader = DatabaseReader.new(database_connection)
 
-  database_reader.read().to_json
+    database_reader.day = day
+
+    database_reader.read().to_json
+  end
 end
 
 get "/week/:year/:month/:day" do
@@ -46,4 +53,22 @@ end
 
 get "/" do
   redirect to("index.html")
+end
+
+def cached(prefix, date)
+  cache_file = ROOT_PATH.join("tmp", "cache", "#{prefix}_#{date.year}_#{date.month}_#{date.day}")
+
+  if date < Date.today
+    if File.exist?(cache_file)
+      File.read(cache_file)
+    else
+      contents = yield
+      File.open(cache_file, "w") do |file|
+        file.write contents
+      end
+      contents
+    end
+  else
+    yield
+  end
 end
