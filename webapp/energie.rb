cc207c8c607ad7476b1f782c372b16b9934a46a0
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2014-12-29 21:32:47 +0100

    Remove unused action /week/*.

--- webapp/energie.rb
+++ webapp/energie.rb
@@ -79,14 +79,6 @@ class Energie < Sinatra::Base
     end
   end
 
-  get "/week/:year/:month/:day" do
-    database_reader = DatabaseReader.new(database_connection)
-
-    database_reader.week = DateTime.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-
-    database_reader.read().to_json
-  end
-
   get "/month/:year/:month" do
     $database.with {|database_connection|
       database_reader = DatabaseReader.new(database_connection)
