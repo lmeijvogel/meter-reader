e0671120c5b6f97fa8732fa7b2d921147ba0bf1c
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2019-07-07 20:42:09 +0200

    Move DatabaseConnectionFactory to separate file.

---
+++ lib/database_connection_factory.rb
@@ -0,0 +1,46 @@
+require 'mysql2'
+
+class DatabaseConnectionFactory
+  def initialize(environment)
+    @environment = environment
+  end
+
+  def with_connection
+    with_retries(log_message_format: "Retrying SQL connection (%d/%d)") do
+      connection = Mysql2::Client.new(DatabaseConfig.for(@environment))
+      begin
+        yield connection
+      ensure
+        connection.close
+      end
+    end
+  end
+
+  private
+
+  def with_retries(max_tries: 5, log_message_format: "Retrying (%d/%d)")
+    tries = 0
+
+    retry_interval_in_seconds = 2
+
+    begin
+      yield
+    rescue Mysql2::Error => e
+      puts "Error: #{e.message}"
+      tries += 1
+
+      if tries > max_tries
+        raise
+      end
+
+      puts format(log_message_format, tries, max_tries)
+      sleep retry_interval_in_seconds
+
+      retry_interval_in_seconds *= 2
+
+      retry
+    end
+  end
+end
+
+
--- runner.rb
+++ runner.rb
@@ -5,6 +5,7 @@ require 'dotenv'
 $LOAD_PATH << File.expand_path("../lib", __FILE__)
 
 require "p1_meter_reader"
+require "database_connection_factory"
 require "output/database_writer"
 require "output/last_measurement_store"
 require "database_config"
@@ -13,46 +14,6 @@ ROOT_PATH = Pathname.new File.dirname(__FILE__)
 
 Dotenv.load
 
-class DatabaseConnectionFactory
-  def initialize(environment)
-    @environment = environment
-  end
-
-  def with_connection
-    with_retries(log_message_format: "Retrying SQL connection (%d/%d)") do
-      connection = Mysql2::Client.new(DatabaseConfig.for(@environment))
-      begin
-        yield connection
-      ensure
-        connection.close
-      end
-    end
-  end
-
-  private
-
-  def with_retries(max_tries: 5, log_message_format: "Retrying (%d/%d)")
-    tries = 0
-    retry_interval = 2
-
-    begin
-      yield
-    rescue Mysql2::Error => e
-      puts "Error: #{e.message}"
-      tries += 1
-
-      if tries > max_tries
-        raise
-      end
-
-      puts format(log_message_format, tries, max_tries)
-      sleep retry_interval
-
-      retry_interval *= 2
-
-      retry
-    end
-  end
 
 end
 
