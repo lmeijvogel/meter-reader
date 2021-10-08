aad4943b106655f3b62696ce794e6b3edd4cf4fd
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2021-10-08 22:39:45 +0200

    Improve development mode console output.

--- lib/p1_meter_reader/data_parsing/fake_stream_splitter.rb
+++ lib/p1_meter_reader/data_parsing/fake_stream_splitter.rb
@@ -1,28 +1,34 @@
 module P1MeterReader
   module DataParsing
     class FakeStreamSplitter
-      def record
-        <<-RECORD
-/XMX5XMXABCE100129872
+      def initialize
+        @stroom_dal = 2018.0
+        @stroom_piek = 2293.0
+        @gas = 2246.0
+      end
 
-0-0:96.1.1(4B414145303031343535343236383133)
-1-0:1.8.1(02018.957*kWh)
-1-0:1.8.2(02293.712*kWh)
-1-0:2.8.1(00000.000*kWh)
-1-0:2.8.2(00000.000*kWh)
-0-0:96.14.0(0002)
-1-0:1.7.0(0000.50*kW)
-1-0:2.7.0(0000.00*kW)
-0-0:17.0.0(999*A)
-0-0:96.3.10(1)
-0-0:96.13.1()
-0-0:96.13.0()
-0-1:96.1.0(3238303131303038333338303831393133)
-0-1:24.1.0(03)
-0-1:24.2.1(150327210000)(00)(60)(1)(2246.014*m3)
-(02246.014)
-0-1:24.4.0(1)
-!
+      def record
+        <<~RECORD
+          /XMX5XMXABCE100129872
+
+          0-0:96.1.1(4B414145303031343535343236383133)
+          1-0:1.8.1(#{pretty_print @stroom_dal }*kWh)
+          1-0:1.8.2(#{pretty_print @stroom_piek }*kWh)
+          1-0:2.8.1(00000.000*kWh)
+          1-0:2.8.2(00000.000*kWh)
+          0-0:96.14.0(0002)
+          1-0:1.7.0(0000.50*kW)
+          1-0:2.7.0(0000.00*kW)
+          0-0:17.0.0(999*A)
+          0-0:96.3.10(1)
+          0-0:96.13.1()
+          0-0:96.13.0()
+          0-1:96.1.0(3238303131303038333338303831393133)
+          0-1:24.1.0(03)
+          0-1:24.2.1(150327210000)(00)(60)(1)(#{pretty_print @gas}*m3)
+          (02246.014)
+          0-1:24.4.0(1)
+          !
         RECORD
       end
 
@@ -32,7 +38,21 @@ module P1MeterReader
 
       def read
         sleep 1
+
+        if Random.rand < 0.05
+          @stroom_piek += (2 * Random.rand)
+        end
+        if Random.rand < 0.05
+          @stroom_dal += (2 * Random.rand)
+        end
+        if Random.rand < 0.05
+          @gas += (2 * Random.rand)
+        end
         return record
+      end
+
+      def pretty_print(value)
+        format("%09.3f", value)
       end
     end
   end
--- lib/water_reader/fake_water_measurement_listener.rb
+++ lib/water_reader/fake_water_measurement_listener.rb
@@ -1,13 +1,17 @@
 module WaterReader
   class FakeWaterMeasurementListener
     def read
-      sleep 3
+      sleep 1
 
       random_event
     end
 
+    def ready?
+      true
+    end
+
     def random_event
-      if rand(10) < 4
+      if rand(10) < 2
         "USAGE"
       else
         "TICK"
