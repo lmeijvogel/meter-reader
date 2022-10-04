7fde93d429a0294ae0331dbd0bab978eeff6ff5f
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-10-04 21:51:00 +0200

    Rename 'measurement' to 'reading' in meter_reader.rb since it's more accurate.

--- meter_reader.rb
+++ meter_reader.rb
@@ -26,32 +26,32 @@ Dotenv.load
 def main
   environment = ENV.fetch('ENVIRONMENT')
 
-  measurement_counter = 0
+  reading_counter = 0
 
   database_connection_factory = DatabaseConnectionFactory.new(environment)
   database_writer = DatabaseWriter.new(database_connection_factory)
   database_writer.save_interval = 5
 
-  recent_measurement_store = RecentMeasurementStore.new(
-    number_of_entries: 8 * 60, # 8 hours at 1 measurement per minute
+  recent_reading_store = RecentMeasurementStore.new(
+    number_of_entries: 8 * 60, # 8 hours at 1 reading per minute
     redis_host: ENV.fetch("REDIS_HOST"),
     redis_list_name: ENV.fetch("REDIS_MEASUREMENTS_LIST_NAME")
   )
 
-  last_measurement = :no_last_measurement
+  last_reading = :no_last_reading
 
-  water_measurement_store = WaterMeasurementStore.new(
+  water_reading_store = WaterMeasurementStore.new(
     redis_host: ENV.fetch("REDIS_HOST"),
   )
 
   if environment == "production"
     stream_splitter = P1MeterReader::DataParsing::StreamSplitter.new(ENV.fetch("P1_CONVERTER_MESSAGE_START"), input: serial_port)
 
-    current_measurement_echoer = ->(measurement) { }
+    current_reading_echoer = ->(reading) { }
   else
     log "Fake stream splitter"
     stream_splitter = P1MeterReader::DataParsing::FakeStreamSplitter.new
-    current_measurement_echoer = ->(measurement) { log "Received measurement: #{measurement}"}
+    current_reading_echoer = ->(reading) { log "Received reading: #{reading}"}
 
     # Timecop.scale(360)
   end
@@ -62,29 +62,29 @@ def main
 
   influx = InfluxDBClient.new(hostname: ENV.fetch("INFLUXDB_HOST"), org: ENV.fetch("INFLUXDB_ORG"), bucket: ENV.fetch("INFLUXDB_BUCKET"), token: ENV.fetch("INFLUXDB_TOKEN"))
 
-  recorder.collect_data do |measurement|
-    if valid?(measurement, last_measurement)
-      measurement.water = water_measurement_store.get
+  recorder.collect_data do |reading|
+    if valid?(reading, last_reading)
+      reading.water = water_reading_store.get
 
-      database_writer.save_unless_exists(measurement)
+      database_writer.save_unless_exists(reading)
 
-      json = measurement_to_json(measurement, measurement_counter)
-      recent_measurement_store.add(json)
+      json = reading_to_json(reading, reading_counter)
+      recent_reading_store.add(json)
 
-      measurement_counter += 1
+      reading_counter += 1
 
       begin
-        if last_measurement != :no_last_measurement
-          influx.send_gas_reading(measurement.gas) if measurement.gas > last_measurement.gas
+        if last_reading != :no_last_reading
+          influx.send_gas_reading(reading.gas) if reading.gas > last_reading.gas
 
-          stroom = measurement.stroom_dal.to_f + measurement.stroom_piek.to_f
-          levering = measurement.levering_dal.to_f + measurement.levering_piek.to_f
-          last_stroom = last_measurement.stroom_dal.to_f + last_measurement.stroom_piek.to_f
-          last_levering = last_measurement.levering_dal.to_f + last_measurement.levering_piek.to_f
+          stroom = reading.stroom_dal.to_f + reading.stroom_piek.to_f
+          levering = reading.levering_dal.to_f + reading.levering_piek.to_f
+          last_stroom = last_reading.stroom_dal.to_f + last_reading.stroom_piek.to_f
+          last_levering = last_reading.levering_dal.to_f + last_reading.levering_piek.to_f
 
           influx.send_stroom_reading(stroom) if stroom > last_stroom
 
-          influx.send_current_reading(measurement.stroom_current) # Always send current
+          influx.send_current_reading(reading.stroom_current) # Always send current
 
           # Water is sent separately in the water runner
         end
@@ -92,53 +92,53 @@ def main
         $stdout.puts "ERROR sending to InfluxDB: #{e.message}"
       end
 
-      last_measurement = measurement
+      last_reading = reading
 
-      # current_measurement_echoer.(measurement)
+      current_reading_echoer.(reading)
     end
   end
 end
 
-def measurement_to_json(measurement, measurement_counter)
-  stroom = measurement.stroom_dal.to_f + measurement.stroom_piek.to_f
-  levering = measurement.levering_dal.to_f + measurement.levering_piek.to_f
+def reading_to_json(reading, reading_counter)
+  stroom = reading.stroom_dal.to_f + reading.stroom_piek.to_f
+  levering = reading.levering_dal.to_f + reading.levering_piek.to_f
 
   {
-    id:               measurement_counter,
-    time_stamp:       measurement.time_stamp.to_s,
-    time_stamp_utc:   measurement.time_stamp_utc.to_s,
+    id:               reading_counter,
+    time_stamp:       reading.time_stamp.to_s,
+    time_stamp_utc:   reading.time_stamp_utc.to_s,
     stroom:           stroom,
     levering:         levering,
-    stroom_current:   measurement.stroom_current.to_f,
-    gas:              measurement.gas.to_f,
-    water:            measurement.water.to_f
+    stroom_current:   reading.stroom_current.to_f,
+    gas:              reading.gas.to_f,
+    water:            reading.water.to_f
   }.to_json
 end
 
-def valid?(measurement, last_measurement)
-  if last_measurement == :no_last_measurement
-    measurement_not_zero?(measurement)
+def valid?(reading, last_reading)
+  if last_reading == :no_last_reading
+    reading_not_zero?(reading)
   else
     # The new meter is a bit less trustworthy and will sometimes report
-    # invalid energy measurements, e.g. 0 or less than it should
-    return false if measurement.stroom_dal.nil? || measurement.stroom_dal.to_f < last_measurement.stroom_dal.to_f
-    return false if measurement.stroom_piek.nil? || measurement.stroom_piek.to_f < last_measurement.stroom_piek.to_f
+    # invalid energy readings, e.g. 0 or less than it should
+    return false if reading.stroom_dal.nil? || reading.stroom_dal.to_f < last_reading.stroom_dal.to_f
+    return false if reading.stroom_piek.nil? || reading.stroom_piek.to_f < last_reading.stroom_piek.to_f
     # Do not enable these yet since there is no levering yet :D
-    # return false if measurement.levering_dal.nil? || measurement.levering_dal.to_f < last_measurement.levering_dal.to_f
-    # return false if measurement.levering_piek.nil? || measurement.levering_piek.to_f < last_measurement.levering_piek.to_f
-    return false if measurement.gas.nil? || measurement.gas.to_f < last_measurement.gas.to_f
+    # return false if reading.levering_dal.nil? || reading.levering_dal.to_f < last_reading.levering_dal.to_f
+    # return false if reading.levering_piek.nil? || reading.levering_piek.to_f < last_reading.levering_piek.to_f
+    return false if reading.gas.nil? || reading.gas.to_f < last_reading.gas.to_f
 
     true
   end
 end
 
-def measurement_not_zero?(measurement)
-  return false if measurement.stroom_dal.nil? || measurement.stroom_dal.to_f.to_i.abs < 0.01
-  return false if measurement.stroom_piek.nil? || measurement.stroom_piek.to_f.to_i.abs < 0.01
+def reading_not_zero?(reading)
+  return false if reading.stroom_dal.nil? || reading.stroom_dal.to_f.to_i.abs < 0.01
+  return false if reading.stroom_piek.nil? || reading.stroom_piek.to_f.to_i.abs < 0.01
   # Do not enable yet
-  # return false if measurement.levering_dal.nil? || measurement.levering_dal.to_f.to_i.abs < 0.01
-  # return false if measurement.levering_piek.nil? || measurement.levering_piek.to_f.to_i.abs < 0.01
-  return false if measurement.gas.nil? || measurement.gas.to_i.abs < 0.01
+  # return false if reading.levering_dal.nil? || reading.levering_dal.to_f.to_i.abs < 0.01
+  # return false if reading.levering_piek.nil? || reading.levering_piek.to_f.to_i.abs < 0.01
+  return false if reading.gas.nil? || reading.gas.to_i.abs < 0.01
 
   true
 end
